dc4188752b1515487051f0240d0c73ab5631176c
Trenton Kress <trenton.kress@gmail.com>
2024-06-01 13:56:55 -0700

    Fix up wintertodt script to dodge damage more consistently and efficiently gather points near the end

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/trent/api/ExtensionGlobals.kt
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/trent/api/ExtensionGlobals.kt
@@ -1,2 +1,7 @@
 package net.runelite.client.plugins.microbot.trent.api
 
+import net.runelite.client.plugins.microbot.util.Global
+
+fun sleepUntil(checkEvery: Int = 300, timeout: Int = 5000, condition: () -> Boolean) {
+    Global.sleepUntilTrue(condition, checkEvery, timeout)
+}
