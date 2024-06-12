b6853d64f607a02757dc50a388bd9c620a75b0f8
Trenton Kress <trenton.kress@gmail.com>
2024-06-11 22:38:05 -0700

    Add barbarian assault plank gatherer

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/trent/barbassaultplankgatherer/BarbAssaultPlankGatherer.kt
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/trent/barbassaultplankgatherer/BarbAssaultPlankGatherer.kt
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.microbot.trent.ardystalls
+package net.runelite.client.plugins.microbot.trent.barbassaultplankgatherer
 
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
@@ -7,33 +7,31 @@ import net.runelite.api.Client
 import net.runelite.api.coords.WorldPoint
 import net.runelite.client.plugins.Plugin
 import net.runelite.client.plugins.PluginDescriptor
+import net.runelite.client.plugins.microbot.Microbot
 import net.runelite.client.plugins.microbot.trent.api.State
 import net.runelite.client.plugins.microbot.trent.api.StateMachineScript
 import net.runelite.client.plugins.microbot.trent.api.bankAt
 import net.runelite.client.plugins.microbot.util.Global.sleep
 import net.runelite.client.plugins.microbot.util.Global.sleepUntil
 import net.runelite.client.plugins.microbot.util.bank.Rs2Bank
-import net.runelite.client.plugins.microbot.util.combat.Rs2Combat
-import net.runelite.client.plugins.microbot.util.gameobject.Rs2GameObject
+import net.runelite.client.plugins.microbot.util.grounditem.Rs2GroundItem
 import net.runelite.client.plugins.microbot.util.inventory.Rs2Inventory
 import net.runelite.client.plugins.microbot.util.player.Rs2Player
 import net.runelite.client.plugins.microbot.util.walker.Rs2Walker
 import javax.inject.Inject
 
-val THIEVING_TILE = WorldPoint(2669, 3310, 0)
-
 @PluginDescriptor(
-    name = PluginDescriptor.Trent + "Ardougne Stall Thiever",
-    description = "Steals and banks cakes in ardougne",
-    tags = ["thieving", "cake", "ardougne"],
+    name = PluginDescriptor.Trent + "Barb Plank Gatherer",
+    description = "Gathers planks at barbarian assault",
+    tags = ["plank"],
     enabledByDefault = false
 )
-class ArdougneStallThiever : Plugin() {
+class BarbAssaultPlankGatherer : Plugin() {
     @Inject
     private lateinit var client: Client
 
     private var running = false
-    private val script = ArdougneStallScript()
+    private val script = BarbAssaultPlankGathererScript()
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun startUp() {
@@ -54,11 +52,16 @@ class ArdougneStallThiever : Plugin() {
     }
 }
 
-class ArdougneStallScript : StateMachineScript() {
+class BarbAssaultPlankGathererScript : StateMachineScript() {
     override fun getStartState(): State {
         return Root()
     }
 }
+
+val WORLDS = intArrayOf(320, 323, 324, 331, 332, 338, 339, 340, 347, 348, 355, 356, 357, 374, 378)
+var currentWorldIndex = 0
+
+val PLANK_LOCATION = WorldPoint(2553, 3577, 0)
 
 private class Root : State() {
     override fun checkNext(client: Client): State? {
@@ -66,16 +69,10 @@ private class Root : State() {
     }
 
     override fun loop(client: Client, script: StateMachineScript) {
-        if (Rs2Combat.inCombatNotBraindamaged()) {
-            Rs2Walker.walkTo(WorldPoint(2656, 3286, 0))
+        if (Rs2Player.isWalking())
             return
-        }
-        if (Rs2Inventory.contains("chocolate slice", "bread")) {
-            Rs2Inventory.dropAll(650, 820, "chocolate slice", "bread")
-            return
-        }
         if (Rs2Inventory.isFull()) {
-            if (bankAt(10355, WorldPoint(2656, 3286, 0))) {
+            if (bankAt(19051, WorldPoint(2537, 3573, 0))) {
                 Rs2Bank.depositAll()
                 sleepUntil { Rs2Inventory.isEmpty() }
                 Rs2Bank.closeBank()
@@ -83,18 +80,26 @@ private class Root : State() {
             }
             return
         }
-        if (!Rs2Player.getWorldLocation().equals(THIEVING_TILE)) {
-            println("walking to thieving tile")
-            if (!Rs2Walker.walkTo(THIEVING_TILE, 1))
-                Rs2Walker.walkFastCanvas(THIEVING_TILE)
+        if (Rs2Player.getWorldLocation().distanceTo(PLANK_LOCATION) >= 7) {
+            if (Rs2Walker.walkMiniMap(PLANK_LOCATION, 3))
+                Rs2Player.waitForWalking()
             return
         }
-        val stall = Rs2GameObject.findObject(11730, WorldPoint(2667, 3310, 0))
-        stall?.let {
-            if (Rs2GameObject.interact(it, "steal-from")) {
-                Rs2Player.waitForAnimation()
-                sleep(600, 850)
+        val planks = Rs2GroundItem.getAll(8).filter { it.item?.name == "Plank" }
+        if (planks.isEmpty()) {
+            hop()
+            return
+        }
+        planks.forEach {
+            if (Rs2GroundItem.interact(it, "take")) {
+                Rs2Player.waitForWalking()
+                sleep(600, 816)
             }
         }
+    }
+
+    fun hop() {
+        Microbot.hopToWorld(WORLDS[currentWorldIndex])
+        currentWorldIndex = (currentWorldIndex + 1) % WORLDS.size
     }
 }
