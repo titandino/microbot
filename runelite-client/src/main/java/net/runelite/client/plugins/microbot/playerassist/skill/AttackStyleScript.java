9f671108315c7fdf202c7fcc5bc63f35ef1e27fd
Trenton Kress <trenton.kress@gmail.com>
2024-06-23 17:50:49 -0700

    Add iron superheater for fossil island camp

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/playerassist/skill/AttackStyleScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/playerassist/skill/AttackStyleScript.java
@@ -91,12 +91,12 @@ public class AttackStyleScript extends Script {
     }
 
     private void changeAttackStyle(PlayerAssistConfig config, WidgetInfo attackStyleWidgetInfo) {
-        if (Rs2Tab.getCurrentTab() != InterfaceTab.COMBAT) {
-            Rs2Tab.switchToCombatOptionsTab();
-            sleepUntil(() -> Rs2Tab.getCurrentTab() == InterfaceTab.COMBAT, 2000);
-        }
-        log.info("Changing Attack Style to: {}", attackStyleToTrain);
-        Rs2Combat.setAttackStyle(attackStyleWidgetInfo);
+        // if (Rs2Tab.getCurrentTab() != InterfaceTab.COMBAT) {
+        //     Rs2Tab.switchToCombatOptionsTab();
+        //     sleepUntil(() -> Rs2Tab.getCurrentTab() == InterfaceTab.COMBAT, 2000);
+        // }
+        // log.info("Changing Attack Style to: {}", attackStyleToTrain);
+        // Rs2Combat.setAttackStyle(attackStyleWidgetInfo);
     }
 
     // has any of the skills leveled up
